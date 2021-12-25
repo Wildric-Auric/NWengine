5df396a9d65cdccf97ec65e4b4ff5f57e3247947
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2021-12-25 01:09:22 +0100

    made Vector class template

--- src/Maths.h
+++ src/Maths.h
@@ -1,16 +1,55 @@
 #ifndef MATHS_H
 #define MATHS_H
 
+template<class T>
 class Vector2 {
 public:
 	float x;
 	float y;
-	Vector2(float x = 0.0f, float y = 0.0f);
+	Vector2(T x = 0.0f, T y = 0.0f);
 private:
 	Vector2 operator + (Vector2 const& vec1);
-	Vector2 operator + (float const& num);
-	Vector2 operator * (float const& num);
+	Vector2 operator + (T const& num);
+	Vector2 operator * (T const& num);
 	Vector2 operator * (Vector2 const& vec1);
-
 };
+
+
+
+	template<class T>
+	Vector2<T>::Vector2(T x, T y) {
+		Vector2::x = x;
+		Vector2::y = y;
+	}
+	template<class T>
+	Vector2<T> Vector2<T>::operator + (Vector2 const& vec1) {
+		Vector2 sum;
+		sum.x = x + vec1.x;
+		sum.y = y + vec1.y;
+		return sum;
+	}
+	template<class T>
+	Vector2<T> Vector2<T>::operator * (Vector2 const& vec1) {
+		Vector2 newVec;
+		newVec.x = x * vec1.x;
+		newVec.y = y * vec1.y;
+		return newVec;
+	}
+	template<class T>
+	Vector2<T> Vector2<T>::operator + (T const& num) {
+		Vector2 newVec;
+		newVec.x = x + num;
+		newVec.y = y + num;
+		return newVec;
+	}
+
+	template<class T>
+	Vector2<T> Vector2<T>::operator * (T const& num) {
+		Vector2 newVec;
+		newVec.x = x * num;
+		newVec.y = y * num;
+		return newVec;
+	}
+
+
 #endif
