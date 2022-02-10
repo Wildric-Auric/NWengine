7e43ea78c418d5c9c45c42d12270e8c8fb1e369c
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-02-10 01:46:14 +0100

    added engine header

---
+++ Engine/NWengine.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+/*
+
+
+					 /$$   /$$ /$$      /$$                               /$$
+					| $$$ | $$| $$  /$ | $$                              |__/
+					| $$$$| $$| $$ /$$$| $$  /$$$$$$  /$$$$$$$   /$$$$$$  /$$ /$$$$$$$   /$$$$$$
+					| $$ $$ $$| $$/$$ $$ $$ /$$__  $$| $$__  $$ /$$__  $$| $$| $$__  $$ /$$__  $$
+					| $$  $$$$| $$$$_  $$$$| $$$$$$$$| $$  \ $$| $$  \ $$| $$| $$  \ $$| $$$$$$$$
+					| $$\  $$$| $$$/ \  $$$| $$_____/| $$  | $$| $$  | $$| $$| $$  | $$| $$_____/
+					| $$ \  $$| $$/   \  $$|  $$$$$$$| $$  | $$|  $$$$$$$| $$| $$  | $$|  $$$$$$$
+					|__/  \__/|__/     \__/ \_______/|__/  |__/ \____  $$|__/|__/  |__/ \_______/
+																/$$  \ $$
+															   |  $$$$$$/
+																\______/
+
+																														*/
+
+
+#include <GL/glew.h>
+#include <glfw3.h>
+#include <iostream>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "Interface.h"
+#include "Globals.h"
+#include "Primitives.h"
+#include "Inputs.h"
+#include "Maths.h"
+#include "Game.h"
+#include "Texture.h"
+#include "Text.h"
+#include "RessourcesLoader.h"
+#include "Camera.h"
+#include "Utilities.h"
+#include "TileMap.h"
+#include "Scene.h"
+#include "Audio.h"
