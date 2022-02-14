e6fb20d674589df928a8e0a97f12758d4c49c9bb
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-02-14 23:34:51 +0100

    enabled player collisions

--- Scripts/player.h
+++ Scripts/player.h
@@ -22,17 +22,17 @@ public:
 	void Update() {
 
 		isGrounded = 0;
-		//for (int i = 0; i < 15; i++) {
-		//	while (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i])) {
-		//		goc->position.y += 1;
-		//		//DevNote::By setting up the collider offset I understood finally what Shaun Spalding used to do with placemeeting...
-		//	
-		//	};
-		//	if (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i], Vector2<int>(0, -1))) {
-		//		isGrounded = 1;
-		//		break;
-		//	}
-		//}
+		for (int i = 0; i < 15; i++) {
+			while (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i])) {
+				goc->position.y += 1;
+				//DevNote::By setting up the collider offset I understood finally what Shaun Spalding used to do with placemeeting...
+			
+			};
+			if (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i], Vector2<int>(0, -1))) {
+				isGrounded = 1;
+				break;
+			}
+		}
 		static float maxJump = 0.0f;
 		//yspd *= 1-isGrounded;
 		if (isGrounded && input_space) {
