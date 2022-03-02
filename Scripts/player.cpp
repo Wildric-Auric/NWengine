4e6152d4f859f7f336946cf61b43a5cd98e82d7b
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-03-03 00:13:17 +0100

    added start inheretiance from scriptable class

---
+++ Scripts/player.cpp
@@ -0,0 +1,45 @@
+#include "player.h"
+#include "NWengine.h"
+#include "Utilities.h"
+extern Collider groundCollider[20];
+
+void player::Update() {
+	isGrounded = 0;
+	for (int i = 0; i < 15; i++) {
+		while (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i])) {
+			goc->position.y += 1;
+			//DevNote::By setting up the collider offset I understood finally what Shaun Spalding used to do with placemeeting...
+
+		};
+		if (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i], Vector2<int>(0, -1))) {
+			isGrounded = 1;
+			break;
+		}
+	}
+	static float maxJump = 0.0f;
+	//yspd *= 1-isGrounded;
+	if (isGrounded && input_space) {
+		isJumping = 1;
+		jumpingStartPosition = goc->position.y;
+		goc->position.y += 1;
+		maxJump = -jumpTime / 2 * (jumpTime / 2 - jumpTime);
+	}
+
+	if (isJumping) {
+		timer += deltaTime;
+
+		yspd = -(jumpHeight / maxJump) * timer * (timer - jumpTime);
+		if (timer > jumpTime) {
+			yspd = 0.0f;
+			isJumping = false;
+			timer = 0.0f;
+		}
+		goc->position.y = jumpingStartPosition + yspd;
+	}
+	goc->position.x += isRunning * 200 * deltaTime;
+	goc->scale.x = sign(isRunning);
+
+
+	float a = joystickAxis[0];
+	isRunning = Clamp((float)(input_right - input_left) + a, -1.0f, 1.0f);
+}
--- Scripts/player.h
+++ Scripts/player.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "NWengine.h"
+#include "GameObject.h"
 
-extern Collider groundCollider[20];
 
-class player : Updatable {
+class player : public Scriptable {
 public:
 	float isRunning = 0.0f;
 	int isJumping = 0;
@@ -13,51 +12,11 @@ public:
 	float jumpTime = 0.8f;
 	float timer = 0.0f;
 	float jumpHeight = 50.0f;
-	GameObjectClone* goc;
 	float yspd = 0.0f;
+	virtual void Start() {};
+	virtual void Update();
 	player(GameObjectClone* goc) {
 		this->goc = goc;
 		goc->AddComponent<Collider>();
-	};
-	void Update() {
-
-		isGrounded = 0;
-		for (int i = 0; i < 15; i++) {
-			while (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i])) {
-				goc->position.y += 1;
-				//DevNote::By setting up the collider offset I understood finally what Shaun Spalding used to do with placemeeting...
-			
-			};
-			if (IsColliding(goc->GetComponent<Collider>(), &groundCollider[i], Vector2<int>(0, -1))) {
-				isGrounded = 1;
-				break;
-			}
-		}
-		static float maxJump = 0.0f;
-		//yspd *= 1-isGrounded;
-		if (isGrounded && input_space) {
-			isJumping = 1;
-			jumpingStartPosition = goc->position.y;
-			goc->position.y += 1;
-			maxJump = -jumpTime / 2 * (jumpTime / 2 - jumpTime);
-		}
-
-		if (isJumping) {
-			timer += deltaTime;
-
-			yspd = -(jumpHeight / maxJump) * timer * (timer - jumpTime);
-			if (timer > jumpTime) {
-				yspd = 0.0f;
-				isJumping = false;
-				timer = 0.0f;
-			}
-			goc->position.y = jumpingStartPosition + yspd;
-		}
-		goc->position.x += isRunning * 200 * deltaTime;
-		goc->scale.x = sign(isRunning);
-
-
-		float a = joystickAxis[0];
-		isRunning = Clamp((float)(input_right - input_left) + a, -1.0f, 1.0f);
 	};
 };
