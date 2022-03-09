10c57a70d12d1dd978c06fe6f2161d33def5a062
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-03-09 18:14:09 +0100

    made jump code more polyvalent for testing

--- Ressources/Scripts/player.cpp
+++ Ressources/Scripts/player.cpp
@@ -2,43 +2,62 @@
 #include "NWengine.h"
 #include "Utilities.h"
 
+
 void player::Update() {
 	isGrounded = 0;
+	int vCol = 0;
+	int direction = (input_right - input_left);
 	for (auto it = Collider::componentList.begin(); it!= Collider::componentList.end(); it++) {
 		if (it->first != goc) {
-			while (IsColliding(goc->GetComponent<Collider>(), &it->second)) {
-				goc->position.y += 1;
-			};
+			if (direction != 0) {
+				if (IsColliding(goc->GetComponent<Collider>(), &it->second, Vector2<int>(direction, 0))) {
+					while (IsColliding(goc->GetComponent<Collider>(), &it->second)) {
+						goc->position.x -= direction;
+					};
+					vCol = direction;
+				};
+			}
+
 			if (IsColliding(goc->GetComponent<Collider>(), &it->second, Vector2<int>(0, -1))) {
+				while (IsColliding(goc->GetComponent<Collider>(), &it->second)) {
+					goc->position.y += 1;
+				};
 				isGrounded = 1;
-				break;
 			}
 		}
 	}
 	static float maxJump = 0.0f;
-	//yspd *= 1-isGrounded;
+
 	if (isGrounded && input_space) {
 		isJumping = 1;
 		jumpingStartPosition = goc->position.y;
 		goc->position.y += 1;
-		maxJump = -jumpTime / 2 * (jumpTime / 2 - jumpTime);
+		//maxJump = -jumpTime / 2 * (jumpTime / 2 - jumpTime);
+		
+		//temp jump code
+		yspd = 5;
+		isGrounded = 0;
 	}
+	yspd -= deltaTime*30;
+	yspd *= 1-isGrounded;
 
-	if (isJumping) {
-		timer += deltaTime;
+	goc->position.y += yspd;
+	//if (isJumping) {
+	//	timer += deltaTime;
 
-		yspd = -(jumpHeight / maxJump) * timer * (timer - jumpTime);
-		if (timer > jumpTime) {
-			yspd = 0.0f;
-			isJumping = false;
-			timer = 0.0f;
-		}
-		goc->position.y = jumpingStartPosition + yspd;
-	}
+	//	yspd = -(jumpHeight / maxJump) * timer * (timer - jumpTime);
+	//	if (timer > jumpTime) {
+	//		yspd = 0.0f;
+	//		isJumping = false;
+	//		timer = 0.0f;
+	//	}
+	//	goc->position.y = jumpingStartPosition + yspd;
+	//}
 	goc->position.x += isRunning * 200 * deltaTime;
 	goc->scale.x = sign(isRunning);
 
 
 	float a = joystickAxis[0];
-	isRunning = Clamp((float)(input_right - input_left) + a, -1.0f, 1.0f);
+	if (direction == vCol) direction = 0;
+	isRunning = Clamp((float)direction + a, -1.0f, 1.0f);
 }
