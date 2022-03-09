3085057a5c2054e015ce2fbc2196b042942960e9
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-03-09 18:13:31 +0100

    fixed collision function

--- Engine/Utilities.h
+++ Engine/Utilities.h
@@ -11,26 +11,48 @@
 inline bool IsColliding(Collider* collider1, Collider* collider2, Vector2<int> offset1 = Vector2<int>(0,0)) {
 	uint8_t overlap = 0;
 	if (collider1 == nullptr || collider2 == nullptr) return false;
-	int minX1 = collider1->GetPosition().x + offset1.x -  collider1->size->x / 2;
-	int maxX1 = collider1->GetPosition().x + offset1.x +  collider1->size->x/2;
-	int minY1 = collider1->GetPosition().y + offset1.y -  collider1->size->y/2;
-	int maxY1 = collider1->GetPosition().y + offset1.y +  collider1->size->y/2;
+	int minX1 = collider1->GetPosition().x + offset1.x -  collider1->GetSize().x / 2;
+	int maxX1 = collider1->GetPosition().x + offset1.x +  collider1->GetSize().x/2;
+	int minY1 = collider1->GetPosition().y + offset1.y -  collider1->GetSize().y/2;
+	int maxY1 = collider1->GetPosition().y + offset1.y +  collider1->GetSize().y/2;
 
-	int minX2 = collider2->GetPosition().x - collider2->size->x / 2;
-	int maxX2 = collider2->GetPosition().x + collider2->size->x / 2;
-	int minY2 = collider2->GetPosition().y - collider2->size->y / 2;
-	int maxY2 = collider2->GetPosition().y + collider2->size->y / 2;
+	int minX2 = collider2->GetPosition().x - collider2->GetSize().x / 2;
+	int maxX2 = collider2->GetPosition().x + collider2->GetSize().x / 2;
+	int minY2 = collider2->GetPosition().y - collider2->GetSize().y / 2;
+	int maxY2 = collider2->GetPosition().y + collider2->GetSize().y / 2;
 
 	overlap += (  
-					( (minX1 < minX2) && (minX2 < maxX1) ) ||  
-					( (minX2 < minX1) && (minX1 < maxX2) ) 
+					( (minX1 <= minX2) && (minX2 <= maxX1) ) ||  
+					( (minX2 <= minX1) && (minX1 <= maxX2) ) 
 			   )
 			+ 
 			   ( 
-					 ( (minY1 < minY2) && (minY2 < maxY1) ) ||
-					 ( (minY2 < minY1) && (minY1 < maxY2) )
+					 ( (minY1 <= minY2) && (minY2 <= maxY1) ) ||
+					 ( (minY2 <= minY1) && (minY1 <= maxY2) )
 			   )
 		;
+
+	//overlap += (((minX1 < maxX2) && (minX1 > minX2)) ||
+	//	((maxX1 < maxX2) && (maxX1 > minX2))
+
+	//	)
+	//	+
+	//	(
+	//		((minY1 < maxY2) && (minY1 > minY2)) ||
+	//		((maxY1 < maxY2) && (maxY1 > minY2))
+
+	//		)
+	//	;
+
+	//overlap1 += (((minX2 < maxX1) && (minX2 > minX1)) ||
+	//	((maxX2 < maxX1) && (maxX2 > minX1))
+	//	) +
+	//	(
+	//		((minY2 < maxY1) && (minY2 > minY1)) ||
+	//		((maxY2 < maxY1) && (maxY2 > minY1))
+	//		);
+
+
 	return overlap == 2;
 }
 
