057a684b4c0bdc5a38d58b454af40df37733b8b5
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-07-03 16:41:21 +0200

    changed variables naming

--- Engine/Inputs.cpp
+++ Engine/Inputs.cpp
@@ -2,9 +2,9 @@
 #include "Globals.h"
 #include<iostream>
 
- bool Inputs::input_left, Inputs::input_right, Inputs::input_up, Inputs::input_down, Inputs::input_d,
- Inputs::input_1, Inputs::input_2, Inputs::input_3, Inputs::input_0, Inputs::input_4, Inputs::input_left_click,
- Inputs::input_space, Inputs::usingJoystick, Inputs::input_f2, Inputs::input_enter
+ bool Inputs::left, Inputs::right, Inputs::up, Inputs::down, Inputs::d,
+ Inputs::n_1, Inputs::n_2, Inputs::n_3, Inputs::n_0, Inputs::n_4, Inputs::left_click,
+ Inputs::space, Inputs::usingJoystick, Inputs::f2, Inputs::enter
  = 0;
 double Inputs::mousePosX;
 double Inputs::mousePosY;
@@ -28,20 +28,20 @@ void Inputs::Process(GLFWwindow* window)
 		}
 	}
 
-	input_left = ( glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS );
-	input_right = glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS;
-	input_up = glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS;
-	input_down = glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS;
-	input_d = glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS;
-	input_left_click = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
-	input_0 = glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS;
-	input_1 = glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS;
-	input_2 = glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS;
-	input_3 = glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS;
-	input_4 = glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS;
-	input_space = ( glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS ) || ( usingJoystick&& GLFW_PRESS == buttons[1] ); 
-	input_f2 = (glfwGetKey(window, GLFW_KEY_F2) == GLFW_PRESS);
-	input_enter = (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS);
+	left = ( glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS );
+	right = glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS;
+	up = glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS;
+	down = glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS;
+	d = glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS;
+	left_click = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+	n_0 = glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS;
+	n_1 = glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS;
+	n_2 = glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS;
+	n_3 = glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS;
+	n_4 = glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS;
+	space = ( glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS ) || ( usingJoystick&& GLFW_PRESS == buttons[1] ); 
+	f2 = (glfwGetKey(window, GLFW_KEY_F2) == GLFW_PRESS);
+	enter = (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS);
 
 
 };
