a16385bc6cbc0d0833413189a5bb9d2fe7d3ca9d
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-07-03 23:30:50 +0200

    update

--- Engine/Gui/DebugGui.h
+++ Engine/Gui/DebugGui.h
@@ -2,7 +2,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 #include "SceneEditorGui.h"
-
+#include "Scene.h"
+#include "Utilities.h"
 class DebugGui {
 public:
 	static bool isActive;
@@ -12,7 +13,13 @@ public:
 		ImGui::Text("fps = %f", Globals::fps);
 		ImGui::DragInt2("cam pos", &SceneEditorGui::cam.position.x);
 		ImGui::DragFloat("zoom", &SceneEditorGui::cam.zoom, 0.1, -0.0, 10.0);
-
+		static bool b = 0;
+		if (ImGui::Button("fuck")) GetFile();
+		ImGui::Checkbox("Save Scene", &b);
+		if (b) {
+			b = 0;
+			Scene::currentScene->Save();
+		}
 		ImGui::Separator();
 		ImGui::ShowDemoWindow();
 		static ImGuiIO& io = ImGui::GetIO(); (void)io;
