05e458ea6c844dbc8eda03b690918ca55ace2cb8
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-07-09 15:26:27 +0200

    added a wrapper for glUse in shader class

--- Engine/Shader.h
+++ Engine/Shader.h
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #define SHADER_DEFAULT "Shaders/Shader1.shader"
+#define SHADER_POST_PROCESSING "Shaders/PostProcessing.shader"
 
 class Shader {
 	public:
@@ -12,12 +13,12 @@ class Shader {
 		std::string name;
 
 		Shader(std::string path = SHADER_DEFAULT);
+		void Use();
 		void SetMat4x4(const char* name, const float* value);
 		void SetUniform1f(const char* name, const float);
 		void SetUniform3f(const char* name, float x, float y, float z);
 		void SetVector2(const char* name, float value0, float value1);
 		void SetUniform1i(const char* name, const int);
-
 		static std::map<std::string, Shader> resList;
 };
 std::pair<const char*, const char*> parseShader(const char* path);
