9d30734fea8d65b19a0d91f0bf6880b611fecfb6
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-09-01 23:01:06 +0200

    implemented engine build through writing to a batch file

--- Build/Builder.cpp
+++ Build/Builder.cpp
@@ -1,10 +1,14 @@
 //This files fills GNUbuild.bat and executes it
+#define PROJECTDIR "C:\\Users\\HP\\Desktop\\NWengine\\"
+
 #include <fstream>
 #include <vector>
 #include <string>
 
+#define UTILDIR "..\\Engine\\STL\\Utilities.h"
+#include UTILDIR
 
-static std::string ProjectDir = "C:\\Users\\HP\\Desktop\\NWengine\\";
+static std::string ProjectDir = PROJECTDIR;
 static std::string IncludeDir[] = {
     "dependencies\\GLEW\\include",
     "dependencies\\GLFW\\include",
@@ -25,13 +29,12 @@ static std::string IncludeDir[] = {
 
 };
 
-
 static std::string LibsDir[] = {
-    "dependencies\\GLFW\\lib-mingw"                                    ,
+    "dependencies\\GLFW\\lib-vc2019"                                   ,
     "dependencies\\GLEW\\lib\\Release\\Win32"                          ,
     "dependencies\\freetype\\release static\\vs2015-2022\\win32"       ,
     "dependencies\\SND\\lib"                                           ,
-    "dependencies\\OPENAL\\win32"
+    "dependencies\\OPENAL\\win32"                                    
 };
 //Don't need absolute path in the future, it should be automatized
 static std::string staticLibs[] = {
@@ -70,7 +73,7 @@ static std::string objs[] = {
     "Engine\\Components\\PostProcessing.cpp"                                                      ,
     "Engine\\Components\\Script.cpp"                                                              ,
     "Engine\\Components\\Transform.cpp"                                                           ,
-    "Engine\\Console.cpp"                                                                         ,
+    "Engine\\Gui\\Console.cpp"                                                                    ,
     "Engine\\STL\\NWstd.cpp"                                                                      ,
     "Engine\\STL\\Parser.cpp"                                                                     ,
     "Engine\\Components\\Particles.cpp"                                                           ,
@@ -79,21 +82,22 @@ static std::string objs[] = {
     "Engine\\Context\\Context.cpp"                                                                ,
     "Engine\\Components\\GameObject.cpp"                                                          ,
     "Engine\\Globals.cpp"                                                                         ,
-    "Engine\\Inputs.cpp"                                                                          ,
+    "Engine\\Context\\Inputs.cpp"                                                                 ,
     "Engine\\Context\\Primitives.cpp"                                                             ,
     "Engine\\NWengine.cpp"                                                                        ,
-    "Engine\\RessourcesLoader.cpp"                                                                ,
+    "Engine\\Context\\RessourcesLoader.cpp"                                                       ,
     "Engine\\Scene.cpp"                                                                           ,
     "Engine\\Shader.cpp"                                                                          ,
     "Engine\\Source.cpp"                                                                          ,
     "Engine\\Text.cpp"                                                                            ,
-    "Engine\\Texture.cpp"                                                                         ,
+    "Engine\\Context\\Texture.cpp"                                                                ,
     "Engine\\TileMap.cpp"                                                                         ,
     "Ressources\\Scripts\\player.cpp"                                                             ,
     "Engine\\Components\\Sprite.cpp"                                                              ,
     "Engine\\Gui\\StaticGuiVariable.cpp"                                                          ,
     "Engine\\Animation.cpp"                                                                       ,
-    "Engine\\Native Scripting\\ScriptManager.cpp"
+    "Engine\\Native Scripting\\ScriptManager.cpp"                                                 ,
+    "Engine\\Components\\Animator.cpp"                                                          
 };
 
 
@@ -102,14 +106,17 @@ std::vector<std::string> o;
 
 std::string PreprocessorMacros[] = {
     "GLEW_STATIC",
-    "WIN32"
+    "WIN32",
+    "_CRT_SECURE_NO_WARNINGS",
+    "_CONSOLE",
+    "_MBCS"
 };
 
 std::string outputDir = "";
 std::string exeName = "BuildTest.exe";
 
 
-static void Build(bool compile, bool link) {
+static void Compile() {
     //Creating objects out of scripts
     std::ofstream ofs("Builder.bat");
     ofs << "@echo off\n";
@@ -119,100 +126,80 @@ static void Build(bool compile, bool link) {
         ofs << "/D" << MAC << " ";
     }
     ofs << "\"\n";
-
-    if (compile) {
         outputDir = ProjectDir + "Build\\Engine Build\\Data\\";
-        for (std::string scr : objs) {
+    for (std::string scr : objs) {
 
-            //Getting it's name
-            std::string filename = "";
-            std::string extension = "";
-            bool state = 0;
-            for (auto chr : scr) {
-                if (chr == '.') {
-                    filename += extension;
-                    extension = "";
-                    state = 1;
-                }
-                if ((int)chr == (int)'\\') {
-                    filename = "";
-                    extension = "";
-                    continue;
-                }
-                if (!state) filename += chr;
-                else extension += chr;
-
+        //Getting it's name
+        std::string filename = "";
+        std::string extension = "";
+        bool state = 0;
+        for (auto chr : scr) {
+            if (chr == '.') {
+                filename += extension;
+                extension = "";
+                state = 1;
             }
-            ofs << "cl ";
-            ofs << "/c ";
-            bool b = 0;
-            for (std::string dir : IncludeDir) {
-                if (b) {
-                    ofs << "^\n ";
-                }
-                ofs << "/I " << "\"" << ProjectDir << dir << "\"";
-                b = 1;
+            if ((int)chr == (int)'\\') {
+                filename = "";
+                extension = "";
+                continue;
+            }
+            if (!state) filename += chr;
+            else extension += chr;
 
-            };
-            ofs << "^\n";
-            ofs << " %macros% ";
-            ofs << "/Fo"  <<"\""<< outputDir << filename << ".obj" << "\" ";
-            //Adding cpp file
-            ofs << "\""<<ProjectDir << scr << "\"";
-   
-            o.push_back(filename + ".obj");
-            ofs << "\n";
-            ofs << "echo " << filename << " object created\n";
         }
-
-        ofs << "\n\n\n";
-        ofs << "echo Objects creation has ended.\n";
-
-
-    }
-
-    if (link) {
-        ofs << "g++ ";
-        for (std::string MAC : PreprocessorMacros) {
-            ofs << " -D " << MAC << " ";
-        }
-
+        ofs << "cl ";
+        ofs << "/c ";
         bool b = 0;
         for (std::string dir : IncludeDir) {
             if (b) {
                 ofs << "^\n ";
             }
-            ofs << "-I " << dir;
-
+            ofs << "/I " << "\"" << ProjectDir << dir << "\"";
             b = 1;
         };
-        ofs << " ";
-        b = 0;
-        for (std::string dir : LibsDir) {
-            if (b) {
-                ofs << "^\n ";
-            }
-            ofs << "-L " << dir;
-            b = 1;
-        }
-        ofs << " ";
-        for (std::string lib : staticLibs) {
-            ofs << lib << "^\n ";
-        }
 
-        for (std::string obj : o) {
-            ofs << outputDir + obj << "^\n ";
-        }
-        ofs << "-o GNUBuild\\NWengine-GNU-Build-2022.exe";
-        ofs << "\necho exe generated";
+        ofs << "^\n";
+        ofs << " %macros% ";
+        ofs << "/Fo"  <<"\""<< outputDir << filename << ".obj" << "\" ";
+        //Adding cpp file
+        ofs << "\""<<ProjectDir << scr << "\"";
+   
+        o.push_back(filename + ".obj");
+        ofs << "\n";
+        ofs << "echo " << filename << " object created\n";
     }
-
+    ofs << "\n\n\n";
+    ofs << "echo Objects creation has ended.\n";
     ofs.close();
 }
+
+static void Link() {
+    std::ofstream ofs("Builder.bat");
+    ofs << "@echo off\n";
+    ofs << "call vcvars32\n";
+    ofs << "LINK ";
+    std::string objectLoc = "Engine Build\\Data\\";
+    for (std::string obj : GetDirFiles(objectLoc)) {
+        ofs << "\"" << objectLoc << obj << "\"" << "^\n ";
+    }
+    for (std::string dir : LibsDir) {
+        ofs << "/LIBPATH:" << "\""<< ProjectDir <<dir<< "\""<< "^\n ";
+    }
+    for (std::string lib : staticLibs) {
+        ofs << lib << "^\n ";
+    }
+    ofs << "/OUT:NWengine-MSVC.exe";
+    ofs << "\necho exe generated";
+    ofs.close();
+ 
+}
+
 
 static void Execute() {
 
 }
 int main() {
-    Build(1, 0);
+    //Compile();
+    Link();
 }
