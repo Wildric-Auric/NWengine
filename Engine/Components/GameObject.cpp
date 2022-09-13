b98493a1cf4de630989c25f10977552a91280b0f
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-09-13 20:10:31 +0200

    fixed merging mistakes

--- Engine/Components/GameObject.cpp
+++ Engine/Components/GameObject.cpp
@@ -24,7 +24,7 @@ void GameObject::Draw(int8 textureSlot) {
 	sprite->shader->SetUniform1f("uTime", Globals::uTime);
 	sprite->shader->SetVector2("uMouse", (float)Inputs::mousePosX, (float)(Inputs::mousePosY));
 	sprite->shader->SetUniform1i("uTex0", textureSlot);
-	sprite->shader->SetVector2("uResolution", (float) Globals::NATIVE_WIDTH, (float) Globals::NATIVE_HEIGHT);
+	sprite->shader->SetVector2("uResolution", (float) Context::NATIVE_WIDTH, (float) Context::NATIVE_HEIGHT);
 	glm::mat4x4 model = glm::translate(glm::mat4(1.0f), glm::vec3((double)position.x, (double)position.y, sprite->zbuffer));
 	model = glm::scale(model, glm::vec3(scale.x, scale.y, 1.0f));
 	sprite->shader->SetMat4x4("uMvp", &(Camera::ActiveCamera->projectionMatrix * Camera::ActiveCamera->viewMatrix * model)[0][0]);
