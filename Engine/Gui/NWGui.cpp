26d74906de072466c4f9ea60d4a9157cbe4cc8f2
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2022-09-17 21:26:29 +0200

    improved gui

--- Engine/Gui/NWGui.cpp
+++ Engine/Gui/NWGui.cpp
@@ -12,5 +12,17 @@ bool NWGui::FileHolder(std::string name, std::string holdedFile) {
 		ImGui::EndTable();
 	}
 	return ret;
+}
 
+bool NWGui::CheckBox(std::string label, bool* value) {
+	bool ret = 0;
+	if (ImGui::BeginTable("testTable0", 2)) {
+		ImGui::TableNextRow();
+		ImGui::TableNextColumn();
+		ImGui::Text(label.c_str());
+		ImGui::TableNextColumn();
+		ret = ImGui::Checkbox((std::string("##")+label).c_str(), value);
+		ImGui::EndTable();
+	}
+	return ret;
 }
--- Engine/Gui/NWGui.h
+++ Engine/Gui/NWGui.h
@@ -2,11 +2,14 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 #include "Globals.h"
-
+#define GUI_SEP ImGui::Separator();
+#define GUI_NEWLINE ImGui::NewLine();
+#define GUI_SAMELINE ImGui::SameLine();
 class NWGui {
 public:
 
 	static bool FileHolder(std::string name, std::string holdedFile);
+	static bool CheckBox(std::string label, bool* value);
 template<typename T>
 	static bool DragValue(const char* label, void* target, ImGuiDataType dataType, int16 vectorSize = 1, float speed = 1.0f, T minn = 0.0f, T maxx = 0.0f) {
 		bool ret = 0;
@@ -19,6 +22,7 @@ template<typename T>
 			ret = ImGui::DragScalarN((std::string("##")+std::string(label)).c_str(), dataType, target, vectorSize, speed, &minn, &maxx);
 			ImGui::EndTable();
 		}
+		
 		return ret;
 	}
 };
