3d3d8c68ad3281cdc54d111c238e0a7b1c58017b
Wildric <wildric.auric@gmail.com>
2022-12-29 17:57:00 +0100

    changed function name to respect name's convention

--- Engine/Reflection/NWReflection.h
+++ Engine/Reflection/NWReflection.h
@@ -10,7 +10,7 @@
 class Reflected {
 	//Map Key is the variable name, First element of the tuple is the size in bytes, the second is the offset, the third is the type, the fourth is a pointer to the variable
 	std::map<std::string, std::tuple<int, int, std::string, void*>> reflectionData;
-	std::tuple<int, int, std::string, void*> getReflectionData(std::string varName) {
+	std::tuple<int, int, std::string, void*> GetReflectionData(std::string varName) {
 		std::map<std::string, std::tuple<int, int, std::string, void*>>::iterator value = this->reflectionData.find(varName);
 		if (value == this->reflectionData.end()) //TODO::Maybe a log?
 			return std::make_tuple<int, int, std::string, void*>(0,0,"", nullptr);
