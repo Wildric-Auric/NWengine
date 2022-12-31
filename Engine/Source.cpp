c4e23e2b5c26087d5c5dafebfce2ec31ef4eb127
Wildric <wildric.auric@gmail.com>
2022-12-31 01:26:19 +0100

    added dll processing

--- Engine/Source.cpp
+++ Engine/Source.cpp
@@ -1,13 +1,31 @@
-#ifdef NW_GAME_BUILD
-	#include "Game.h"
-#else
-	#include "NWengine.h"
+#ifndef _WINDLL	
+
+	#ifdef NW_GAME_BUILD
+		#include "Game.h"
+	#else
+		#include "NWengine.h"
+	#endif
+
+
+	int main()
+	{
+
+	#ifdef NW_GAME_BUILD
+		Game::Run();
+
+	#else
+		#ifdef NW_DLL_ENGINE
+			//TODO::Find out how to create window in exe and process eveything in dll...
+			int response = NW_RELOAD_DLL;
+			while (response != NW_SHUTDOWN_DLL) {
+				DllHandle engine = DllHandle("NWengine.dll");
+				int(*DllRunFunc)() = (int(*)()) GetDllFunction(&engine, "DllRun");
+				response = (*DllRunFunc)();
+			}
+		#else
+			NWengine::Run();
+			
+		#endif
+	#endif
+	};
 #endif
-int main()
-{	
-#ifdef NW_GAME_BUILD
-	Game::Run();
-#else
-	NWengine::Run();
-#endif
-};
