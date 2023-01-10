d43f880918cb9aa049d3f50dc74deae0001a4b09
Wildric <wildric.auric@gmail.com>
2023-01-10 13:37:15 +0100

    added play mode buttons

--- Engine/Gui/SceneViewGui.h
+++ Engine/Gui/SceneViewGui.h
@@ -6,6 +6,12 @@
 #include "Texture.h"
 #include "PostProcessing.h"
 
+#define SCENE_VIEW_PLAY_ICON "Ressources\\Images\\Icons\\Play.png"
+#define SCENE_VIEW_NEXT_ICON "Ressources\\Images\\Icons\\Next.png"
+#define SCENE_VIEW_PAUSE_ICON "Ressources\\Images\\Icons\\Pause.png"
+#define SCENE_VIEW_STOP_ICON "Ressources\\Images\\Icons\\Stop.png"
+
+
 
 class SceneViewGui {
 
@@ -14,12 +20,47 @@ public:
 
 	static void Show() {
 		if (!isActive) return;
+
 		ImGui::Begin("Scene", &isActive, ImGuiWindowFlags_MenuBar);
 		if (Camera::ActiveCamera == nullptr) { ImGui::End(); return;}
+		
 		uint32 texture = Camera::ActiveCamera->fbo.RenderedImage.texture;
 		PostProcessing* pp = Camera::ActiveCamera->attachedObj->GetComponent<PostProcessing>();
 
+
+
+
 		if (pp != nullptr) texture = pp->fbo.RenderedImage.texture;
+
+		uint32 playBut = RessourcesLoader::LoadTexture(SCENE_VIEW_PLAY_ICON)->texture;
+		if (Globals::PLAY_MODE) {
+			playBut = RessourcesLoader::LoadTexture(SCENE_VIEW_STOP_ICON)->texture;
+		}
+
+		uint32 nextBut = RessourcesLoader::LoadTexture(SCENE_VIEW_NEXT_ICON)->texture;
+		uint32 pauseBut= RessourcesLoader::LoadTexture(SCENE_VIEW_PAUSE_ICON)->texture;
+
+		if (ImGui::ImageButton((void*)(intptr_t)
+			playBut,
+			ImVec2(30, 30),
+			ImVec2(0, 1), ImVec2(1, 0))) {
+			Globals::PLAY_MODE = 1 - Globals::PLAY_MODE;
+		}
+
+		ImGui::SameLine();
+
+		if (ImGui::ImageButton((void*)(intptr_t)
+			pauseBut,
+			ImVec2(30, 30),
+			ImVec2(0, 1), ImVec2(1, 0))) Console::Write("Test0");
+
+		ImGui::SameLine();
+
+		if (ImGui::ImageButton((void*)(intptr_t)
+			nextBut,
+			ImVec2(30, 30),
+			ImVec2(0, 1), ImVec2(1, 0))) Console::Write("Test1");
+
 		ImGui::Image((void*)(intptr_t)
 			texture,
 			ImVec2(Camera::ActiveCamera->fbo.RenderedImage.size.x, Camera::ActiveCamera->fbo.RenderedImage.size.y),
