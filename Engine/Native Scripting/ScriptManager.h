4db742cb90913d4223cdd0dd89dcc06723a717d5
Wildric <wildric.auric@gmail.com>
2023-02-05 22:16:14 +0100

    added scripts compilation code

--- Engine/Native Scripting/ScriptManager.h
+++ Engine/Native Scripting/ScriptManager.h
@@ -8,9 +8,16 @@
 class ScriptManager {
 public:
 	static std::map<std::string, Scriptable* (*)(GameObject*)> ScriptsMap;
+	static std::map<std::string, std::string> scriptList;
+
 	static Scriptable* GetScriptFromDLL(std::string name, GameObject* go);
 	static Scriptable* GetScriptFromHeaderFile(std::string name, GameObject* go);
 	static Scriptable* CreateScript(std::string name, GameObject* go);
+
+	static void SaveScriptList();
+	static void LoadScriptList();
+	static bool CompileScripts(); //Should return 0 if action has failed
+	static bool CompileScript(std::string element);
 };
 
 
