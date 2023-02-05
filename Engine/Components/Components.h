2c4bd298fdb6087d319965159a9c7d1705679b89
Wildric <wildric.auric@gmail.com>
2023-02-05 22:10:18 +0100

    commented code causing linking error in scripts

--- Engine/Components/Components.h
+++ Engine/Components/Components.h
@@ -4,7 +4,7 @@
 #include "ParticleSystem.h"
 #include "Transform.h"
 #include "Sprite.h"
-#include "Camera.h"
+//#include "Camera.h" //Glfw error if included in scripts LATE TODO::Find the reason
 #include "AudioEmitter.h"
 #include "AudioListener.h"
 #include "Animator.h"
