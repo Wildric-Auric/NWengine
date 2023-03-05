d51f5508c406d56a3a566a7ebb90e23c23ba257d
Wildric <wildric.auric@gmail.com>
2023-03-05 17:08:59 +0100

    added text backend

--- Engine/Text.h
+++ Engine/Text.h
@@ -1,29 +1,39 @@
-//#pragma once
-//
-//#include "Text.h"
-//#include <ft2build.h>
-//#include FT_FREETYPE_H
-//#include <iostream>
-//#include <map>
-//#include "Maths.h"
-//#include "ShaderManager.h"
-//#include "string"
-//
-//struct Character {
-//	unsigned int id;
-//	Vector2<int> size;
-//	Vector2<int> bearing;
-//	unsigned int advance;
-//};
-//
-//class Font {
-//private:
-//	FT_Library ft;
-//	FT_Face face;
-//	unsigned int VAO, VBO;
-//public:
-//	Font(const char* font);
-//	std::map<char, Character> chars;
-//	void DisplayText(std::string text, Vector2<int> position, Shader* shader, Vector3<float> textColor, uint8_t slot = 0);
-//	
-//};
+#pragma once
+#include "ft2build.h"
+#include FT_FREETYPE_H
+#include <string>
+#include <unordered_map>
+
+#include "GameObject.h"
+#include "Texture.h"
+
+class TextSystem {
+private:
+public:
+	static FT_Library lib;
+	static bool Init();
+	static void Destroy();
+};
+
+class Glyph {
+public:
+	Glyph();
+	GameObject go;
+	Texture    texture;
+	uint32 advance;
+	iVec2  bearing;
+	iVec2  size;
+};
+
+class Font {
+private:
+	FT_Face face;
+public:
+	std::unordered_map<char, Glyph> charactersMap;
+	Font(){};
+	Font(std::string path);
+	bool LoadFont(std::string path);
+	void Delete();
+};
+
+
