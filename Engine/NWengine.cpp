448071777f1380e4f43b6ef24598cc5ba3ad2671
Wildric <wildric.auric@gmail.com>
2023-11-07 23:13:27 +0100

    implemented new entry point system, cleaned up main core header file

--- Engine/NWengine.cpp
+++ Engine/NWengine.cpp
@@ -1,161 +1,100 @@
 #include "NWengine.h"
-#include "NWGui.h"
-#include "Gui.h"
-#include "SceneEditor.h"
-#include "GL/glew.h"
-#include "glfw3.h"
 
-#ifdef _WINDLL
-extern "C"
-{
-	int DllLoop() {
-		int frameCount = 0;
-		double currentTime;
-		double lastTime;
-		double deltaTimeSum = 0;
-		lastTime = glfwGetTime();
-
-		GameObject renderObj = GameObject();
-		Renderer::defaultRenderer = new Renderer(&renderObj); //Unchanged by the user
-		Renderer::currentRenderer = Renderer::defaultRenderer;
-
-		Context::dllFlag = NW_KEEP_DLL_RUNNING;
-		while (Context::dllFlag == NW_KEEP_DLL_RUNNING) {
-			Gui::Begin();
-			Context::Clear();
-			Inputs::Process((GLFWwindow*)Context::window);
-			Gui::Update();
-			Camera::Update();
-			Scene::Update();
-			Renderer::currentRenderer->CaptureOnCamFrame();
-
-			Gui::Render();
-
-			Context::Update();
-			//Updating dll flag
-			if (glfwWindowShouldClose((GLFWwindow*)Context::window)) Context::dllFlag = NW_SHUTDOWN_DLL;
-			NWTime::Update();
-		}
-		return Context::dllFlag;
-	}
-
-	__declspec(dllexport)  int DllRun() {
-		GLFWwindow* window = (GLFWwindow*)Context::InitContext(Context::WINDOW_WIDTH, Context::WINDOW_HEIGHT);
-
-		Globals::SetInstallationDir(GetCurrentDir());
-		(NWproj::currentProj = new NWproj(NWproj::GetCurrentProjFromInstallationDir()))->Load();
-		Globals::SetProjDir(NWproj::currentProj->dir);
-
-		Gui::Init((void*)window);
-		if (!SoundSystem::InitOpenAL()) return -1;
-		if (!TextSystem::Init())
-			return -1;
-		Primitives::Init();
-		RessourcesLoader::LoadDefaultRessources();
-		ScriptManager::LoadScriptList();
-		Context::EnableBlend();
-		SceneEditor::Init();
-		Batch::ComputeIndices();
-		Batch::maxBatchTextures = 32; 
-
-		////Initialization finished
-		if (NWproj::currentProj != nullptr && NWproj::currentProj->defaultScenePath != "")
-			(Scene::currentScene = new Scene(NWproj::currentProj->defaultScenePath))->LoadScene();
-	    DllLoop();
-		NWengine::Shutdown();
-		return Context::dllFlag;
-	}
-}
-
-#endif
-
-#ifndef NW_DLL_ENGINE
-int NWengine::Run() {
-		GLFWwindow* window = (GLFWwindow*)Context::InitContext(Context::WINDOW_WIDTH, Context::WINDOW_HEIGHT);
-		if (window == nullptr) return -1;
-		//Init project
-
-		Globals::SetInstallationDir(GetCurrentDir());
-		(NWproj::currentProj = new NWproj(NWproj::GetCurrentProjFromInstallationDir()))->Load();
-		Globals::SetProjDir(NWproj::currentProj->dir);
-
-		//init imgui
-		Gui::Init((void*)window);
-		//init OpenAL
-		if (!SoundSystem::InitOpenAL()) return -1;
-		//init freetype
-		if (!TextSystem::Init())
-			return -1;
-		
-
-		Primitives::Init();
-		//Load ressources
-		RessourcesLoader::LoadDefaultRessources();
-		//Loading script list
-		ScriptManager::LoadScriptList();
-		//Context settings
-
-		Context::EnableBlend();
-
-		SceneEditor::Init();
-		
-		Batch::ComputeIndices();
-		
-		Batch::maxBatchTextures = 32; //TODO::Make a function that uses glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS...
-
-		////Initialization finished
-		if (NWproj::currentProj != nullptr && NWproj::currentProj->defaultScenePath != "")
-			(Scene::currentScene = new Scene(NWproj::currentProj->defaultScenePath))->LoadScene();
-		
-		NWengine::MainLoop();
-
-		NWengine::Shutdown();
-		return 0;
-}
+#define NW_CALL_EX(locatinon) for (void(*func)() : functionMap[locatinon]) func();
 
 
-//------------------
-void NWengine::MainLoop() {
+std::unordered_map<ON_MAIN_CALL_LOCATION, std::vector<void(*)()>> functionMap;
+GameObject NWengineDefaultRendererContainer;
 
 
-	GameObject renderObj = GameObject();
-	Renderer::defaultRenderer = new Renderer(&renderObj); //Unchanged by the user
+NW_PREFIX int NWengineInit() {
+	void* window = Context::InitContext(Context::WINDOW_WIDTH, Context::WINDOW_HEIGHT);
+	if (window == nullptr) return -1;
+
+	NW_CALL_EX(ON_MAIN_CALL_LOCATION::InitBegin)
+	
+	//init InitSoundSystem
+	if (!SoundSystem::InitOpenAL()) return -1;
+	//init Text System
+	if (!TextSystem::Init())
+		return -1;
+	Primitives::Init();
+	//Context settings
+	Context::EnableBlend();
+	Batch::ComputeIndices();
+	Batch::maxBatchTextures = 32; //TODO::Make a function that uses glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS...
+	//Init Renderer
+	Renderer::defaultRenderer = new Renderer(&NWengineDefaultRendererContainer); //Unchanged by the user
 	Renderer::currentRenderer = Renderer::defaultRenderer;
 
-	while (!glfwWindowShouldClose((GLFWwindow*)Context::window)) {
-		Gui::Begin();
-		//Clearing for the UI
-		Context::Clear();	
-		Inputs::Process((GLFWwindow*)Context::window);
-		Gui::Update();
+	NW_CALL_EX(ON_MAIN_CALL_LOCATION::InitEnd)
+
+	return 0;
+}
+
+NW_NO_MANGLING void NWengineLoop() {
+	while (!Context::ShouldClose()) {
+		//Initializing imgui here for example
+		NW_CALL_EX(ON_MAIN_CALL_LOCATION::FrameBegin)
+		//Clearing the context
+		Context::Clear();
+		Inputs::Process(Context::window);
 		Camera::UpdateActiveCamera();
 		Scene::UpdateActiveScene();
-		//Updating Renderer camera framebuffer
-		Renderer::currentRenderer->CaptureOnCamFrame();
-		//Render Im::Gui
-		Gui::Render();
+		//Any rendering should be done here
+		NW_CALL_EX(ON_MAIN_CALL_LOCATION::FrameIntermediate)
+
 		//Update window and capturing inputs
 		Context::Update();
+	
+		//Updating imgui for example
+		NW_CALL_EX(ON_MAIN_CALL_LOCATION::FrameEnd)
 		//Calculate fps
 		NWTime::Update();
 	}
 }
 
+NW_NO_MANGLING void NWengineShutdown() {
+	NW_CALL_EX(ON_MAIN_CALL_LOCATION::Destroy)
 
-void NWengine::Shutdown() {
-		delete Scene::currentScene;
-		delete Renderer::defaultRenderer;
-		delete SceneEditor::cam;
-		delete NWproj::currentProj;
-
-		ScriptManager::SaveScriptList();
-		SoundSystem::DestroyOpenAL(); 
-
-		TextSystem::Destroy();
-		Primitives::Destroy();
-
-		Gui::Destroy();
-		Context::Destroy();
+	delete Scene::currentScene;
+	delete Renderer::defaultRenderer;
+	SoundSystem::DestroyOpenAL();
+	TextSystem::Destroy();
+	Primitives::Destroy();
+	Context::Destroy();
 }
 
-#endif
+NW_NO_MANGLING bool NWenginePushFunction(ON_MAIN_CALL_LOCATION loc, void(*func)()) {
+	if ((void*)func == nullptr)
+		return 0;
+	std::vector<void(*)()>& vec = functionMap[loc];
+	for (void(*e)() : vec) {
+		if (e == func)
+			return 0;
+	}
+	vec.push_back(func);
+	return 1;
+}
+
+NW_NO_MANGLING bool NWenginePopFunction(ON_MAIN_CALL_LOCATION loc, void(*func)()) {
+	auto pair = functionMap.find(loc);
+	if (pair == functionMap.end() || pair->second.size() <= 0)
+		return 0;
+	if ((void*)func == nullptr) {
+		pair->second.pop_back();
+		return 1;
+	}
+	for (auto iter = pair->second.begin(); iter != pair->second.end(); iter++) {
+		if (*iter == func) {
+			pair->second.erase(iter);
+			return 1; //Pay attention if you change this in the future you may get invalid access because we increment afterwards
+		}
+	}
+	return 0;
+}
+
+#undef NW_CALL_EX
+#undef NW_NO_MANGLING
+#undef NW_EXPORT    
+#undef NW_PREFIX
--- Engine/NWengine.h
+++ Engine/NWengine.h
@@ -15,39 +15,50 @@
 									           |  $$$$$$/
 										    \______/
 
-																														*/
+*/
+
+
+#include "Primitives.h"
+#include "Inputs.h"
+#include "Context.h"
+#include "Camera.h"
+#include "Renderer.h"
+#include "Scene.h"
+#include "Components.h"
+#include "Batch.h"
+#include "Text.h"
+#include "NWproj.h"
 
 #include "Utilities.h"
 #include "Globals.h"
 #include "NWTime.h"
 
-#ifndef NW_DLL_ENGINE
 
-#include <iostream>
-#include "Primitives.h"
-#include "Inputs.h"
-#include "Context.h"
-#include "RessourcesLoader.h"
-#include "Camera.h"
-#include "Renderer.h"
-#include "Scene.h"
-#include "Components.h"
-#include "ScriptManager.h"
 
-#include "Batch.h"
-#include "Text.h"
-#include "NWproj.h"
 
-namespace NWengine {
-	int Run();
-	void MainLoop();
-	void Shutdown();
-}
-
+#define NW_NO_MANGLING extern "C"
+#ifdef NW_DLL
+#define NW_EXPORT     __declspec(dllexport)
+#else
+#define NW_EXPORT
 #endif
+#define NW_PREFIX NW_NO_MANGLING NW_EXPORT
 
-#ifdef _WINDLL
-	extern "C" {
-		__declspec(dllexport) int DllRun();
-	}
-#endif
+enum class ON_MAIN_CALL_LOCATION {
+	
+	InitBegin,			//Executed just after context initialization
+	InitEnd,			//Executed at the end of engine init, after initialization of engine system
+	FrameBegin,			//Executed at beginning of each frame
+	FrameIntermediate,  //Executed each frame just before updating context buffer
+	FrameEnd,			//Executed each frame after updating context 
+	Destroy				//Called before destruction of engine systems
+};
+
+extern std::unordered_map<ON_MAIN_CALL_LOCATION, std::vector<void(*)()>> functionMap;
+extern GameObject NWengineDefaultRendererContainer;
+
+NW_PREFIX int  NWengineInit();
+NW_PREFIX void NWengineLoop();
+NW_PREFIX void NWengineShutdown();
+NW_PREFIX bool NWenginePushFunction(ON_MAIN_CALL_LOCATION loc, void(*func)());
+NW_PREFIX bool NWenginePopFunction(ON_MAIN_CALL_LOCATION loc, void(*func)() = nullptr);
