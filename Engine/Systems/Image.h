cd7b8b64879921ebda1c52946199285af257ac0b
Oussama <wildric.auric@gmail.com>
2024-04-04 00:17:41 +0200

    added new constructor

--- Engine/Systems/Image.h
+++ Engine/Systems/Image.h
@@ -15,6 +15,9 @@ public:
 	int height		   = 0;
 	bool alpha		   = 0;
 	
+	Image() = default;
+	Image(const std::string& x, uint8* buf, int ch, int w, int h, bool a) : path{x}, pixelBuffer{buf}, channels{ch},
+	width{w}, height{h}, alpha{a} {};
 	Asset* LoadFromFile(const char* path, void* output) override;
 	void   Clean()                                      override;
 };
