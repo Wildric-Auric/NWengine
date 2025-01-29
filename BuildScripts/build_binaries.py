332e02af06830782f498c2d890b4a616d6ffd457
Oussama <wildric.auric@gmail.com>
2025-01-29 14:30:55 +0100

    clean

--- BuildScripts/build_binaries.py
+++ BuildScripts/build_binaries.py
@@ -20,25 +20,14 @@ def genHdrs():
             dst = f"{hdrDir}\\temp\\{file}"
             if (len(file) > 2 and file[len(file) - 2::] == ".h") or (len(file) > 4 and file[len(file) - 4::] == ".hpp"):
                 os.system(f'copy "{filepath}" "{dst}"')
-
-#    os.system(f"xcopy ..\\dependencies\\freetype\\include {hdrDir}\\temp /s /e /i")
-#    os.system(f"xcopy ..\\dependencies\\GLEW\\include {hdrDir}\\temp /s /e /i")
-#    os.system(f"xcopy ..\\dependencies\\OPENAL\\include {hdrDir}\\temp /s /e /i")
-#    os.system(f"xcopy ..\\dependencies\\SND\\include {hdrDir}\\temp /s /e /i")
-#    os.system(f"xcopy ..\\dependencies\\vendor\\stb {hdrDir}\\temp  /s /e /i")
-
     asm = f'hdrasm "{hdrDir}temp\\\\" "{hdrDir}" NWengineCore.h NWengine.h'
     os.system(asm)
     os.system(f"rm -rf {hdrDir}\\temp")
 
 
 def buildBin():
-    version = '"1.0.234567"'
-    mac = f';PreprocessorDefinitions="NW_VERSION={version};$(PreprocessorDefinitions)"'
     dbg = "msbuild ..\\Build\\NWengineCore.vcxproj -p:Configuration=Debug;Platform=x64"
     rls = "msbuild ..\\Build\\NWengineCore.vcxproj -p:Configuration=Release;Platform=x64"
-    dbg += mac
-    rls += mac
     os.system(dbg)
     os.system(rls)
 
