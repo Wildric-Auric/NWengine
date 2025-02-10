c00a6efad1e088e8a0fcb56825968512a356a61b
Oussama <wildric.auric@gmail.com>
2025-02-10 01:26:22 +0100

    moved copy to build script

--- BuildScripts/bld_sndbx.bat
+++ BuildScripts/bld_sndbx.bat
@@ -4,3 +4,4 @@ if not defined VisualStudioVersion (
     call vcvars64.bat
 )
 msbuild ..\\Build\\Sandbox.vcxproj -p:Configuration=Debug;Platform=x64
+mv ..\Build\Bin\Sandbox\Debug64\Sandbox.exe ..\Build\Sandbox.exe
--- BuildScripts/run_sndbx.bat
+++ BuildScripts/run_sndbx.bat
@@ -1,4 +1,3 @@
 @echo off
 cd %~dp0%
-mv ..\Build\Bin\Sandbox\Debug64\Sandbox.exe ..\Build\Sandbox.exe
 start cmd /k ..\Build\Sandbox.exe
