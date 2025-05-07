16ce3811456f5ade105c853d987408b1a15aaf0b
Wildric-Auric <wildric.auric@gmail.com>
2025-05-07 17:46:08 +0200

    test update

--- Sandbox/src/templates/GeometryTst.h
+++ Sandbox/src/templates/GeometryTst.h
@@ -2,17 +2,34 @@
 #include "NWengine.h"
 #include "Scene.h"
 #include "LineRenderer.h"
+#include "CircleRenderer.h"
+#include "Sprite.h"
 
 namespace GeometryTst {
 
 struct Data {
-    LineRenderer* lines[8];
+	LineRenderer*	lines[10];
+	CircleRenderer* pts[10];
+	Sprite*			markers[10];
 };
 
 v4f exts[8] = {
-    {0.0,0.0, 56.0,-56.0},
-    {-53.0, -50, 100,50}
-};
+	// 0: diagonal down-right (slope -1)
+	{0.0f, 0.0f, 56.0f, -56.0f},
+	// 1: diagonal up-right (slope ≈ 0.653)
+	{-53.0f, -50.0f, 100.0f, 50.0f},
+	// 2: horizontal at y=10
+	{10.0f, 10.0f, 200.0f, 10.0f},
+	// 3: horizontal at y=10, overlapping 2
+	{50.0f, 10.0f, 150.0f, 10.0f},
+	// 4: horizontal at y=10, disjoint from 2
+	{300.0f, 10.0f, 400.0f, 10.0f},
+	// 5: vertical at x=400
+	{400.0f, 0.0f, 400.0f, 800.0f},
+	// 6: horizontal at y=400, intersects 5
+	{0.0f, 400.0f, 800.0f, 400.0f},
+	// 7: diagonal slope +1, perpendicular to 0 but far apart
+	{500.0f, 500.0f, 600.0f, 600.0f}};
 
 Data data;
 
@@ -25,25 +42,32 @@ static void Init() {
 	Camera*		camC = cam.AddComponent<Camera>();
 	camC->Use();
 	camC->SetClearColor(fVec4(0.2, 0.0, 1.0, 1.0));
-	camC->ChangeOrtho(720, 480);
-    //-------------- 
-    for (int i = 0; i < 2; i++) {
-        data.lines[i] = s.AddObject().AddComponent<LineRenderer>();
-        data.lines[i]->SetWidth(5);
-        data.lines[i]->SetExt({exts[i].x, exts[i].y}, {exts[i].z, exts[i].w});
-    }
+	camC->ChangeOrtho(800, 800);
+	//--------------
+	for(int i = 0; i < 8; i++) {
+		data.lines[i] = s.AddObject().AddComponent<LineRenderer>();
+		data.lines[i]->SetWidth(2);
+		data.lines[i]->SetExt({exts[i].x, exts[i].y}, {exts[i].z, exts[i].w});
+	}
 
+//	for(int i = 0; i < 10; i++) {
+//		data.pts[i] = s.AddObject().AddComponent<CircleRenderer>();
+//		data.pts[i]->SetRadius(5);
+//	}
+//
+//	for(int i = 0; i < 10; i++) {
+//		GameObject& obj = s.AddObject();
+//		obj.AddComponent<Transform>();
+//		data.markers[i] = obj.AddComponent<Sprite>();
+//		data.markers[i]->SetSize({10, 10});
+//	}
 
-    //-------------
+	//-------------
 	s.Start();
 	printf("NW_VERSION: %s\n", NWengineGetVersionString());
 }
 
-static void Render() { 
-        
-
-    (*Renderer::defaultRenderer)(true); 
-}
+static void Render() { (*Renderer::defaultRenderer)(true); }
 
 void Run() {
 	Context::_glInfo.maxVersion = 4;
@@ -54,4 +78,4 @@ void Run() {
 	NWengineLoop();
 	NWengineShutdown();
 }
-}; // namespace Simple
+}; // namespace GeometryTst
