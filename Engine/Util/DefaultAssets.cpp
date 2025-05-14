209a33ef15993c45f9da2373db935df3a04835e2
Wildric-Auric <wildric.auric@gmail.com>
2025-05-14 02:58:19 +0200

    added color uniform to default text shader

--- Engine/Util/DefaultAssets.cpp
+++ Engine/Util/DefaultAssets.cpp
@@ -71,9 +71,9 @@ const ShaderText ShaderTextDefaultStr = {ShaderDefVert,
         uniform sampler2D uTex0;
         in vec2 uv;
         out vec4 FragColor;
-        vec3 color = vec3(1.0);
+        uniform vec4 uCol = vec4(1.0);
         void main(){
-            vec4 col = vec4(color,texture(uTex0, vec2(uv.x, 1.0 - uv.y)).x);
+            vec4 col = vec4(uCol.xyz, texture(uTex0, vec2(uv.x, 1.0 - uv.y)).x * uCol.a);
             FragColor = col;
         }
 )V0G0N"};
