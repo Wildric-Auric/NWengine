99a3cea773f58c094ea8dede5830f12c9ca908ca
Oussama Soudassi <wildric.auric@gmail.com>
2025-12-23 10:14:32 +0100

    fixed directory change when running scripts

--- BuildScripts/bld_sndbx.bat
+++ BuildScripts/bld_sndbx.bat
@@ -1,7 +1,8 @@
 @echo off
-cd %~dp0%
+pushd %~dp0%
 if not defined VisualStudioVersion (
     call vcvars64.bat
 )
 msbuild ..\\Build\\Sandbox.vcxproj -p:Configuration=Debug;Platform=x64
 mv ..\Build\Bin\Sandbox\Debug64\Sandbox.exe ..\Build\Sandbox.exe
+popd
